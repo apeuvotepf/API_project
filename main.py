0db1d69ea19f61869733024d6a2db5319cc0c9a7
apeuvotepf <101734721+apeuvotepf@users.noreply.github.com>
2022-12-04 19:14:07 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from module.movie import Movie
-from module.movie_facade import MovieFacade
-
-movies = MovieFacade.get_movies()
-
-for movie in movies:
-    assert isinstance(movie, Movie)
-    
-print("ok")
